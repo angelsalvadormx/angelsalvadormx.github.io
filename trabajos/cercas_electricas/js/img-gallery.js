f9003595c3d98a34706b033016994c3aacad4aac
Angel Salvador C <Angel.Gabriel.Castaneda@ibm.com>
2020-07-09 18:52:33 -0500

    update cercas_electricas

--- trabajos/cercas_electricas/js/img-gallery.js
+++ trabajos/cercas_electricas/js/img-gallery.js
@@ -1,20 +1,28 @@
-const Elemenetportafolio = document.getElementById('portafolio');
-const Pictures = Elemenetportafolio.getElementsByClassName('icon-expand');
+const Elemenetportafolio = document.getElementById('portfolio');
+const Pictures = Elemenetportafolio.getElementsByClassName('item-portfolio');
 const Background = document.getElementById('background');
+const BtnClose = document.getElementById('close');
 const ImgBack = Background.getElementsByTagName('div')[0];
+const Body = document.getElementsByTagName('body')[0];
 
 Object.keys(Pictures).forEach(key =>{
     Pictures[key].addEventListener('click', function(e){
         Background.classList.add('active');
+        Body.style.overflow = "hidden";
         var UrlImg = this.style.backgroundImage;
         ImgBack.style.backgroundImage= UrlImg
     })
 });
 
-Background.addEventListener('click', e=>{
-    if(e.target !== e.currentTarget) return
-    Background.classList.remove('active')
+function close(Background) {
+    Background.classList.remove('active');
+    Body.style.overflow = "visible";   
+}
+
+BtnClose.addEventListener('click',function(){close(Background)},false);
+Background.addEventListener('click',function(e){
+    if(e.target !== this){
+        return;
+    }
+    close(Background);    
 })
-
-
-``
