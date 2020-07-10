0ff438306ebc9397bea7b59cc1313963cef571e5
Angel Salvador C <Angel.Gabriel.Castaneda@ibm.com>
2020-07-09 23:15:02 -0500

    updates

--- proyectos/btn-animation/js/scriptCopy.js
+++ proyectos/btn-animation/js/scriptCopy.js
@@ -1,1 +1,32 @@
-const buttons = document.getElementsByTagName('button')
+const copyButtons = document.getElementsByClassName('copy');
+
+Object.keys(copyButtons).forEach(function(key) {
+    copyButtons[key].addEventListener('click',copy);
+    copyButtons[key].addEventListener("mouseout", removeClass);
+});
+
+function copy(){
+    const html = this.parentNode.getElementsByClassName('codeToCopy')[0].innerHTML;
+    var doc = new DOMParser().parseFromString(html, 'text/html');
+    var text = '';
+    if(doc.body.firstChild.tagName !== 'BUTTON'){
+        text = doc.body.textContent
+    }else{
+        text = doc.body.innerHTML;
+    }
+    executeCopy(text);   
+    this.classList.add('copied'); 
+}
+
+function executeCopy(text){
+    var input = document.createElement('textarea');
+    document.body.appendChild(input);
+    input.value = text;
+    input.select();
+    document.execCommand('Copy');
+    input.remove();
+}
+
+function removeClass(){
+    this.classList.remove('copied');
+}
